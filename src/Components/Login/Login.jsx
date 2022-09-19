b8fe0d41602013b84d60d29d71d50fbf27c3da31
Sunny Saurav (GreyMatter Development) <sunny.saurav@greyorange.com>
2022-09-19 15:59:43 +0000

    GM-47977 - Login FLow

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,65 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { LoginForm } from 'operational-component-lib';
-import { fetchSeatModeAction } from '../../redux/actions/initialActions';
+import { LoginForm } from "operational-component-lib";
+import { fetchSeatModeAction } from "../../redux/actions/initialActions";
+import videoSrc from "./videoHome.m4v";
 
-function Login({ login }){
-    const dispatch = useDispatch();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [seatName, setSeatName] = useState('');
-    const [ppsSeats, setPpsSeats] = useState([]);
-    const [seatMode, setSeatMode] = useState('');
-    const [isLoggedin, setIsLoggedin] = useState(false);
-    const { pps_seats, mode, success, configs } = useSelector( state => {
-        console.log('state.initialConfigs,', state.initialConfigs);
-        return state.initialConfigs
-    } );
-    console.log()
-    console.log('configs', mode);
-    useEffect(()=>{
-        let listForDropdown = []
-        if(pps_seats.length > 0){
-            pps_seats?.map(i => listForDropdown.push({key: i, value: i}))
-        }
-        setPpsSeats(listForDropdown);
-        if(success) setSeatMode(mode)
-    }, [ pps_seats, mode ])
+const Login = ({ login }) => {
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [seatName, setSeatName] = useState("");
+  const [ppsSeats, setPpsSeats] = useState([]);
+  const [seatMode, setSeatMode] = useState("");
 
-    const handleLogin = (e) => {
-        e.preventDefault()
-        login(username, password, seatName, seatMode);
+  const { pps_seats, mode, success, configs } = useSelector((state) => {
+    return state.initialConfigs;
+  });
+
+  useEffect(() => {
+    let listForDropdown = [];
+    if (pps_seats.length > 0) {
+      pps_seats?.map((i) => listForDropdown.push({ key: i, value: i }));
     }
-    const onChangeHandler = (e) => {
-        const { target : { name, value } } = e;
-        console.log('name, value', name, value);
-        if(name ==='username') setUsername(value)
-        if(name ==='password') setPassword(value)
-        if(name ==='pps_seats') {
-            setSeatName(value)
-            dispatch(fetchSeatModeAction(value, configs, pps_seats))
-        }
-    }
-    const onLoginHandler = (event,username,password) => {
-        console.log(event,username,password)
-    }
+    setPpsSeats(listForDropdown);
+    if (success) setSeatMode(mode);
+  }, [pps_seats, mode]);
 
-    return (
-        <div style={{ paddingBottom: '100px' }}>
-            {/* <form method="post">
-                <select onChange={(e) => {onChangeHandler(e)}} value={seatMode}>
-                    {ppsSeats && ppsSeats.length>0 && ppsSeats.map((seat, i)=>{
-                        return <option key={seat} value={seat}>{seat}</option>
-                    })}
-                </select>
-                <input name="username" type="text" onChange={ (e) => { setUsername(e.target.value) } }/>
-                <input name="password" type="password" onChange={ (e) => { setPassword(e.target.value) } }/>
-                <button type="login" onClick={(e) => { handleLogin(e) }}>Login</button>
-            </form> */}
-            <LoginForm title={'Login'} ppsList={ppsSeats} selectedPps={seatName} username={username} password={password} onChangeHandler={onChangeHandler} onLoginHandler={handleLogin}/>
-        </div>
-    )
-}
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login(username, password, seatName, seatMode);
+  };
 
-export default Login
+  const onChangeHandler = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "username") setUsername(value);
+    if (name === "password") setPassword(value);
+    if (name === "pps_seats") {
+      setSeatName(value);
+      dispatch(fetchSeatModeAction(value, configs, pps_seats));
+    }
+  };
+
+  return (
+    <>
+      <div style={{ margin: "6em 0 0 14em", height: "56em", position: "relative" }}>
+        <video id="background-video" autoPlay muted loop>
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+        <LoginForm
+          title={"Login"}
+          ppsList={ppsSeats}
+          selectedPps={seatName}
+          username={username}
+          password={password}
+          onChangeHandler={onChangeHandler}
+          onLoginHandler={handleLogin}
+          height={"600px"}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Login;
