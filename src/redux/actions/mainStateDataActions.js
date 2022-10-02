8243e752c1c5afbbc48d80d5bbd86608092d4a1b
lakshay.b <lakshay.b@greyorange.com>
2022-10-02 17:58:36 +0530

    GM-56683

--- src/redux/actions/mainStateDataActions.js
+++ src/redux/actions/mainStateDataActions.js
@@ -42,13 +42,6 @@ export function handleUpdateStateDataRequest() {
   };
 }
 
-export const updateStateData = (data) => {
-    return (dispatch)=>{
-        if(data){
-            console.log('state data --', data);
-            const dummyData = PLACE_ENTITY_DATA
-            if(data.state_data) return dispatch(handleUpdateStateDataSuccess(data));
-
 export function handleUpdateStateDataClear(err) {
   return {
     type: UPDATE_STATE_DATA_CLEAR,
