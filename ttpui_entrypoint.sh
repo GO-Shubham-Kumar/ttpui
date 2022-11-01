93f48d505107b9ef847993a586de33f7810dbdd0
Sudivya Thakkar <sudivya.t@greyorange.com>
2022-11-01 04:49:31 +0000

    GM-52843: Deployment Issue

---
+++ ttpui_entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /usr/share/nginx/html/
+mkdir -p globalConfig
+rm -f globalConfig/env-config.js
+touch globalConfig/env-config.js
+
+echo "window['globalConfigs'] = {" >> globalConfig/env-config.js
+
+export | grep UI_REACT_APP_ | awk '{print $3}' | sed -e 's/^.\{,13\}//' -e 's/\"//g' > .env
+
+# Read .env file (key-value pairs)
+while read -r line || [[ -n "$line" ]];
+do
+  # Split env variables by character `=`
+  if printf '%s\n' "$line" | grep -q -e '='; then
+    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  fi
+
+  # Read value of current variable if exists as Environment variable
+  value=$(printf '%s\n' "${!varname}")
+  # else use value from .env file
+  [[ -z $value ]] && value=${varvalue}
+  
+  # Append to JS file
+  echo "  $varname: \"$value\"," >> globalConfig/env-config.js
+done < .env
+
+echo "}" >> globalConfig/env-config.js
